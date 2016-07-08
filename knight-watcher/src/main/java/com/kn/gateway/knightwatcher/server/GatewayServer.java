de1af4f0bf81c2cc86233f8213ac419fbec72cd1
kirshnachaitanya.gumma <chaitanya415@gmail.com>
2016-07-09 02:23:59 +0530

    Initial commit Gateway module

---
+++ knight-watcher/src/main/java/com/kn/gateway/knightwatcher/server/GatewayServer.java
@@ -0,0 +1,26 @@
+package com.kn.gateway.knightwatcher.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by kirshnachaitanya.gumma on 27/04/16.
+ */
+@SpringBootApplication(scanBasePackages = {"com.kn.gateway.knightwatcher.**.config"})
+@EnableZuulProxy
+public class GatewayServer implements HealthIndicator{
+
+    public static void main(String args[])
+    {
+        SpringApplication.run(GatewayServer.class,args);
+    }
+
+    @Override
+    public Health health() {
+        return Health.up().withDetail("STATUS", "SUCCESS").build();
+    }
+}
